ce3f997bcdf9ab27c9d9ddd973f1c20e705824d3
Kevin Oliveira <me@kevinoliveira.com.br>
2017-04-15 10:50:43 -0300

    create tasks to css and vendor css

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,44 @@
+'use strict';
+
 const gulp = require('gulp'),
       sass = require('gulp-sass'),
+      cleancss = require('gulp-cleancss'),
+      concat = require('gulp-concat'),
       pug  = require('gulp-pug');
 
+const files = {
+  css: ['src/assets/sass/main.sass'],
+  cssVendor: [
+    'node_modules/normalize.css/normalize.css'
+  ],
+  js: [''],
+  jsVendor: ['']
+}
+
 gulp.task('views', function() {
-  gulp.src('src/*.pug')
+  return gulp.src('src/*.pug')
     .pipe(pug())
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['views']);
+gulp.task('css', function() {
+  return gulp.src(files.css)
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }))
+    .pipe(cleancss({
+      compatibility: '*'
+    }))
+    .pipe(gulp.dest('dist/assets/css'));
+});
+
+gulp.task('cssVendor', function() {
+  return gulp.src(files.cssVendor)
+    .pipe(concat('vendor.css'))
+    .pipe(cleancss({
+      compatibility: '*'
+    }))
+    .pipe(gulp.dest('dist/assets/css'));
+});
+
+gulp.task('default', ['views', 'css', 'cssVendor']);
